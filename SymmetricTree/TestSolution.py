5032a6740ee5e98fb93a15b1d17a480c42144fb6
Arthur Liang <arthurliang5@gmail.com>
2014-11-16 21:44:20 +0800

    init SymmetricTree

---
+++ SymmetricTree/TestSolution.py
@@ -0,0 +1,28 @@
+'''
+Created on Nov 16, 2014
+
+@author: Arthur
+'''
+import unittest
+import TreeNode
+import Solution
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        self.assertEqual(1, 0, "FAIL")
+
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
---
+++ SymmetricTree/TreeNode.py
@@ -0,0 +1,10 @@
+'''
+Created on Nov 16, 2014
+
+@author: Arthur
+'''
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
