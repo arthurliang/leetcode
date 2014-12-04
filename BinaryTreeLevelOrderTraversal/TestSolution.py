9f9ca4e61862153d4b2b726de2455951081a41ea
Arthur Liang <arthurliang5@gmail.com>
2014-12-04 15:53:41 +0800

    init BinaryTreeLevelOrderTraversal project

---
+++ BinaryTreeLevelOrderTraversal/TestSolution.py
@@ -0,0 +1,33 @@
+'''
+Created on Dec 4, 2014
+
+@author: Arthur
+'''
+import unittest
+import Solution
+import BinTree
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        self.testedobj = Solution.Solution()
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testSolution(self):
+        # arrange
+
+        # act
+
+        # assert
+        self.assertEqual(True, False, "Test")
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
