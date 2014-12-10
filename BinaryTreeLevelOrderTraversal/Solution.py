009ea3e464867be7dc33654aae3e851cceea42df
Arthur Liang <arthurliang5@gmail.com>
2014-12-10 15:22:24 +0800

    add first solution for BinaryTreeLevelOrderTraversal

--- BinaryTreeLevelOrderTraversal/Solution.py
+++ BinaryTreeLevelOrderTraversal/Solution.py
@@ -5,8 +5,40 @@
 #         self.left = None
 #         self.right = None
 
+import queue
 class Solution:
     # @param root, a tree node
     # @return a list of lists of integers
     def levelOrder(self, root):
-        pass
+
+        # TODO: need to know what should be return (None or []) when root is None
+        rst = []
+
+        tq = queue.Queue()
+        tq.put(root)
+
+        tempLevelList, tq = self.getLevelList(tq)
+        while tempLevelList != None:
+            rst.append(tempLevelList)
+            tempLevelList, tq = self.getLevelList(tq)
+
+        return rst
+
+    def getLevelList(self, curlevelqueue):
+        if curlevelqueue.empty():
+            return None, None
+
+        levelvalrst = []
+        nextlevelqueue = queue.Queue()
+        while not curlevelqueue.empty():
+            temptn = curlevelqueue.get()
+            if temptn == None:
+                return None, None
+            if temptn.left != None:
+                nextlevelqueue.put(temptn.left)
+            if temptn.right != None:
+                nextlevelqueue.put(temptn.right)
+            levelvalrst.append(temptn.val)
+
+        return levelvalrst, nextlevelqueue
+
--- BinaryTreeLevelOrderTraversal/TestSolution.py
+++ BinaryTreeLevelOrderTraversal/TestSolution.py
@@ -19,13 +19,75 @@ class Test(unittest.TestCase):
         pass
 
 
-    def testSolution(self):
+    def testlevelOrderForNullTree(self):
         # arrange
+        expRst = []
 
         # act
+        actRslt = self.testedobj.levelOrder(None)
 
         # assert
-        self.assertEqual(True, False, "Test")
+        self.assertEqual(expRst, actRslt)
+
+
+    def testlevelOrderForOneLevel(self):
+        # arrange
+        testdataTrue = "{1}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        expRst = [[1]]
+
+        # act
+        actRslt = self.testedobj.levelOrder(bt.rootnode)
+
+        # assert
+        self.assertEqual(expRst, actRslt)
+
+
+    def testlevelOrderForTwoLevel(self):
+        # arrange
+        testdataTrue = "{1,2,3}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        expRst = [[1],[2,3]]
+
+        # act
+        actRslt = self.testedobj.levelOrder(bt.rootnode)
+
+        # assert
+        self.assertEqual(expRst, actRslt)
+
+
+    def testlevelOrderForTwoLevelWithNullLeft(self):
+        # arrange
+        testdataTrue = "{1,#,3}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        expRst = [[1],[3]]
+
+        # act
+        actRslt = self.testedobj.levelOrder(bt.rootnode)
+
+        # assert
+        self.assertEqual(expRst, actRslt)
+
+
+    def testlevelOrderForFullCase(self):
+        # arrange
+        testdataTrue = "{3,9,20,#,#,15,7}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        expRst = [[3],[9,20],[15,7]]
+
+        # act
+        actRslt = self.testedobj.levelOrder(bt.rootnode)
+
+        # assert
+        self.assertEqual(expRst, actRslt)
 
 
 if __name__ == "__main__":
