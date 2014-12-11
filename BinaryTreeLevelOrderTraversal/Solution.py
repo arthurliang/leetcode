135dd60f5d6ac63e102813ef01b51688956e3ccf
Arthur Liang <arthurliang5@gmail.com>
2014-12-11 10:00:18 +0800

    replace queue lib by list which simulated queue
    Using 'import queue' will be result in the 'Compile Error' in Leetcode
    OJ

--- BinaryTreeLevelOrderTraversal/Solution.py
+++ BinaryTreeLevelOrderTraversal/Solution.py
@@ -5,39 +5,35 @@
 #         self.left = None
 #         self.right = None
 
-import queue
 class Solution:
     # @param root, a tree node
     # @return a list of lists of integers
     def levelOrder(self, root):
-
-        # TODO: need to know what should be return (None or []) when root is None
         rst = []
+        tempQueue = []
+        tempQueue.append(root)
 
-        tq = queue.Queue()
-        tq.put(root)
-
-        tempLevelList, tq = self.getLevelList(tq)
+        tempLevelList, tempQueue = self.getLevelList(tempQueue)
         while tempLevelList != None:
             rst.append(tempLevelList)
-            tempLevelList, tq = self.getLevelList(tq)
+            tempLevelList, tempQueue = self.getLevelList(tempQueue)
 
         return rst
 
     def getLevelList(self, curlevelqueue):
-        if curlevelqueue.empty():
+        if not curlevelqueue:
             return None, None
 
         levelvalrst = []
-        nextlevelqueue = queue.Queue()
-        while not curlevelqueue.empty():
-            temptn = curlevelqueue.get()
+        nextlevelqueue = []
+        while curlevelqueue:
+            temptn = curlevelqueue.pop(0)
             if temptn == None:
                 return None, None
             if temptn.left != None:
-                nextlevelqueue.put(temptn.left)
+                nextlevelqueue.append(temptn.left)
             if temptn.right != None:
-                nextlevelqueue.put(temptn.right)
+                nextlevelqueue.append(temptn.right)
             levelvalrst.append(temptn.val)
 
         return levelvalrst, nextlevelqueue
