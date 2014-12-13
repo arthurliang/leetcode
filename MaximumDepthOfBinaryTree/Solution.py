45aa2714d8e8b3a62517de133037e10fd6bde270
Arthur Liang <arthurliang5@gmail.com>
2014-12-13 21:15:45 +0800

    init MaximumDepthOfBinaryTree project

---
+++ MaximumDepthOfBinaryTree/Solution.py
@@ -0,0 +1,18 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return an integer
+    def maxDepth(self, root):
+        if root is None:
+            return 0
+
+        leftDepth = self.maxDepth(root.left)
+        rightDepth = self.maxDepth(root.right)
+
+        return max(leftDepth + 1, rightDepth + 1)
