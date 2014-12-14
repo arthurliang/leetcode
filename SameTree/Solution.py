f324ef82d4e6fb19506a3d947a3526e9a72aab16
Arthur Liang <arthurliang5@gmail.com>
2014-12-14 19:21:05 +0800

    add solution for SameTree

--- SameTree/Solution.py
+++ SameTree/Solution.py
@@ -10,4 +10,15 @@ class Solution:
     # @param q, a tree node
     # @return a boolean
     def isSameTree(self, p, q):
-        return True
+        if p is None and q is None:
+            return True
+        if p is None and q is not None:
+            return False
+        if q is None and p is not None:
+            return False
+
+        valCompare = (p.val == q.val)
+        leftTreeCompare = self.isSameTree(p.left, q.left)
+        rightTreeCompare = self.isSameTree(p.right, q.right)
+
+        return valCompare and leftTreeCompare and rightTreeCompare
--- SameTree/TestSolution.py
+++ SameTree/TestSolution.py
@@ -28,6 +28,87 @@ class Test(unittest.TestCase):
         # assert
         self.assertEqual(True, actRslt)
 
+
+    def testIsSameTreeWhen1NoneTreeAsLeftPara(self):
+        # arrange
+        testdataTrue = "{1}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        # act
+        actRslt = self.testedobj.isSameTree(None, bt.rootnode)
+
+        # assert
+        self.assertEqual(False, actRslt)
+
+
+    def testIsSameTreeWhen1NoneTreeAsRightPara(self):
+        # arrange
+        testdataTrue = "{1}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        # act
+        actRslt = self.testedobj.isSameTree(bt.rootnode, None)
+
+        # assert
+        self.assertEqual(False, actRslt)
+
+
+    def testIsSameTreeWhen2BinaryTreeNotNone(self):
+        # arrange
+        testdataTrue = "{1}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        # act
+        actRslt = self.testedobj.isSameTree(bt.rootnode, bt.rootnode)
+
+        # assert
+        self.assertEqual(True, actRslt)
+
+
+    def testIsSameTreeWhen2BinaryTreeNotNoneWithSameLeftSubTree(self):
+        # arrange
+        testdataTrue = "{1,2}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        # act
+        actRslt = self.testedobj.isSameTree(bt.rootnode, bt.rootnode)
+
+        # assert
+        self.assertEqual(True, actRslt)
+
+
+    def testIsSameTreeWhen2BinaryTreeNotNoneWithSameRightSubTree(self):
+        # arrange
+        testdataTrue = "{1,#,2}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        # act
+        actRslt = self.testedobj.isSameTree(bt.rootnode, bt.rootnode)
+
+        # assert
+        self.assertEqual(True, actRslt)
+
+
+    def testIsSameTreeWhen2BinaryTreeNotNoneWithDifferentLeftRightSubTree(self):
+        # arrange
+        testdataTrue = "{1,2}"
+        btwlt = BinTree.BinTree()
+        btwlt.DeserializationOnOJ(testdataTrue)
+        testdataTrue = "{1,#,2}"
+        btwrt = BinTree.BinTree()
+        btwrt.DeserializationOnOJ(testdataTrue)
+
+        # act
+        actRslt = self.testedobj.isSameTree(btwlt.rootnode, btwrt.rootnode)
+
+        # assert
+        self.assertEqual(False, actRslt)
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
