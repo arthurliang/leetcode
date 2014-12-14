8a3ce6e8d52ca223f3d9b6f674d24757dd2acfa6
Arthur Liang <arthurliang5@gmail.com>
2014-12-14 22:13:35 +0800

    add solution for BalancedBinaryTree

--- BalancedBinaryTree/Solution.py
+++ BalancedBinaryTree/Solution.py
@@ -11,3 +11,18 @@ class Solution:
     def isBalanced(self, root):
         if root is None:
             return True
+
+        if self.isBalanced(root.left) and self.isBalanced(root.right) \
+           and abs(self.maxDepth(root.left) - self.maxDepth(root.right)) <= 1:
+            return True
+
+        return False
+
+    def maxDepth(self, root):
+        if root is None:
+            return 0
+
+        leftDepth = self.maxDepth(root.left)
+        rightDepth = self.maxDepth(root.right)
+
+        return max(leftDepth, rightDepth) + 1
--- BalancedBinaryTree/TestSolution.py
+++ BalancedBinaryTree/TestSolution.py
@@ -29,6 +29,58 @@ class Test(unittest.TestCase):
         self.assertEqual(True, actRslt)
 
 
+    def testIsBalancedWhenRootIsNotNoneAndLeftRightDepthNotLessThanOne(self):
+        # arrange
+        testdataTrue = "{1,2,#,3}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        # act
+        actRslt = self.testedobj.isBalanced(bt.rootnode)
+
+        # assert
+        self.assertEqual(False, actRslt)
+
+
+    def testIsBalancedWhenRootIsNotNoneAndLeftRightTreeIsBothBalancedAndLeftRightDepthLessThanOne(self):
+        # arrange
+        testdataTrue = "{1}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        # act
+        actRslt = self.testedobj.isBalanced(bt.rootnode)
+
+        # assert
+        self.assertEqual(True, actRslt)
+
+
+    def testIsBalancedWhenRootIsNotNoneAndLeftRightTreeIsBothBalancedButLeftRightTreeDepthNotLessThanOne(self):
+        # arrange
+        testdataTrue = "{1,2,4,3,5,#,#,6}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        # act
+        actRslt = self.testedobj.isBalanced(bt.rootnode)
+
+        # assert
+        self.assertEqual(False, actRslt)
+
+
+    def testIsBalancedWhenRootIsNotNoneAndOneOfLeftRightTreeIsNotBalanced(self):
+        # arrange
+        testdataTrue = "{1,2,4,3,5,#,#,6,#,#,#,7}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        # act
+        actRslt = self.testedobj.isBalanced(bt.rootnode)
+
+        # assert
+        self.assertEqual(False, actRslt)
+
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
