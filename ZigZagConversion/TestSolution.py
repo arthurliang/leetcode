2bffd61ace1a8c081bcfb0141bec6ada42970fad
Arthur Liang <arthurliang5@gmail.com>
2014-12-19 11:49:32 +0800

    add test cases for nRows is same as letters number of given string

--- ZigZagConversion/TestSolution.py
+++ ZigZagConversion/TestSolution.py
@@ -58,6 +58,32 @@ class Test(unittest.TestCase):
         self.assertEqual(expRslt, actRslt)
 
 
+    def testConvertWhenStringHasNlettersAndNRowisN_TC1(self):
+        # arrange
+        s = "a"
+        nRows = 1
+        expRslt = s
+
+        # act
+        actRslt = self.testedobj.convert(s, nRows)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testConvertWhenStringHasNlettersAndNRowisN_TC2(self):
+        # arrange
+        s = "ab"
+        nRows = 2
+        expRslt = s
+
+        # act
+        actRslt = self.testedobj.convert(s, nRows)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
