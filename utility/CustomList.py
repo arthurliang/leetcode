a83e34891fbb5e1f18976fc6d393cd754e7135f4
Arthur Liang <arthurliang5@gmail.com>
2014-12-31 21:50:38 +0800

    completed simple SinglyLinkedList

--- utility/CustomList.py
+++ utility/CustomList.py
@@ -14,7 +14,34 @@ class SinglyLinkedList:
     def __init__(self):
         self.listhead = None
 
+
     def DeserializationOnOJ(self, srlztnOnOJ):
         if srlztnOnOJ == "{}":
             return True
 
+        datalist = srlztnOnOJ[1:-1].split(',')
+
+        self.listhead = ListNode(None)
+        currentPointer = self.listhead
+
+        for index in range(len(datalist)):
+            currentPointer.val = int(datalist[index])
+            if index != len(datalist) - 1:
+                currentPointer.next = ListNode(None)
+                currentPointer = currentPointer.next
+
+        return True
+
+
+    def SerializationOnOJ(self):
+        srlztnOnOJ = "{"
+
+        currentPointer = self.listhead
+
+        while currentPointer is not None:
+            srlztnOnOJ += str(currentPointer.val)
+            currentPointer = currentPointer.next
+            if currentPointer is not None:
+                srlztnOnOJ += ','
+
+        return srlztnOnOJ + '}'
--- utility/TestCustomList.py
+++ utility/TestCustomList.py
@@ -36,6 +36,48 @@ class Test(unittest.TestCase):
         self.testSinglyLinkedListIsNullAfterConstruct()
 
 
+    def testSinglyLinkedListDeserializationOnOJforSimpleCase(self):
+        # arrange
+        srlztn = "{1,2}"
+
+        # act
+        rulst = self.testedobj.DeserializationOnOJ(srlztn)
+
+        # assert
+        self.assertEqual(rulst, True)
+
+        self.assertNotEqual(self.testedobj.listhead, None)
+        self.assertEqual(self.testedobj.listhead.val, 1)
+
+        self.assertNotEqual(self.testedobj.listhead.next, None)
+        self.assertEqual(self.testedobj.listhead.next.val, 2)
+        self.assertEqual(self.testedobj.listhead.next.next, None)
+
+
+    def testSinglyLinkedListSerializationOnOJforNullList(self):
+        # arrange
+        expSrlztn = "{}"
+
+        # act
+        actSrlztn = self.testedobj.SerializationOnOJ()
+
+        # assert
+        self.assertEqual(expSrlztn, actSrlztn)
+
+
+    def testSinglyLinkedListSerializationOnOJforSimpleCase(self):
+        # arrange
+        expSrlztn = "{1,2}"
+        rulst = self.testedobj.DeserializationOnOJ(expSrlztn)
+        self.assertEqual(rulst, True)
+
+        # act
+        actSrlztn = self.testedobj.SerializationOnOJ()
+
+        # assert
+        self.assertEqual(expSrlztn, actSrlztn)
+
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
