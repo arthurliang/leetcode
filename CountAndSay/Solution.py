22b548f07d7bf21258d3ad510afd1a0232b0a522
Arthur Liang <arthurliang5@gmail.com>
2015-01-01 21:25:14 +0800

    add solution for CountAndSay

--- CountAndSay/Solution.py
+++ CountAndSay/Solution.py
@@ -1,4 +1,36 @@
 class Solution:
     # @return a string
     def countAndSay(self, n):
-        return "11"
+        base = ""
+        while n != 0:
+            base = self.caountAndSayStr(base)
+            n -= 1
+
+        return base
+
+
+    def caountAndSayStr(self, s):
+        if s == "":
+            return "1"
+        if s == "1":
+            return "11"
+
+        rslt = ""
+        num = 1
+        cur = s[0]
+
+        index = 1
+        sl = len(s)
+
+        while index != sl:
+            if s[index] == cur:
+                num += 1
+            else:
+                rslt += str(num) + cur
+                num = 1
+                cur = s[index]
+            index += 1
+
+        rslt += str(num) + cur
+
+        return rslt
--- CountAndSay/TestSolution.py
+++ CountAndSay/TestSolution.py
@@ -22,7 +22,79 @@ class Test(unittest.TestCase):
     def testCountAndSayWhenNis1(self):
         #arrange
         n = 1
+        expRslt = "1"
+
+        #act
+        actRslt = self.testedobj.countAndSay(n)
+
+        #assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testCountAndSayWhenNis2(self):
+        #arrange
+        n = 2
         expRslt = "11"
+
+        #act
+        actRslt = self.testedobj.countAndSay(n)
+
+        #assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testCountAndSayWhenNis3(self):
+        #arrange
+        n = 3
+        expRslt = "21"
+
+        #act
+        actRslt = self.testedobj.countAndSay(n)
+
+        #assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testCountAndSayWhenNis4(self):
+        #arrange
+        n = 4
+        expRslt = "1211"
+
+        #act
+        actRslt = self.testedobj.countAndSay(n)
+
+        #assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testCountAndSayWhenNis5(self):
+        #arrange
+        n = 5
+        expRslt = "111221"
+
+        #act
+        actRslt = self.testedobj.countAndSay(n)
+
+        #assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testCountAndSayWhenNi6(self):
+        #arrange
+        n = 6
+        expRslt = "312211"
+
+        #act
+        actRslt = self.testedobj.countAndSay(n)
+
+        #assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testCountAndSayWhenNis7(self):
+        #arrange
+        n = 7
+        expRslt = "13112221"
 
         #act
         actRslt = self.testedobj.countAndSay(n)
