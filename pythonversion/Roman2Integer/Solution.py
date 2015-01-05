17d28ab0f3707884f6eb6970a66e278de84e9c4a
Arthur Liang <arthurliang5@gmail.com>
2015-01-05 22:32:38 +0800

    add solution for Roman2Integer

--- pythonversion/Roman2Integer/Solution.py
+++ pythonversion/Roman2Integer/Solution.py
@@ -5,4 +5,35 @@
 class Solution:
     # @return an integer
     def romanToInt(self, s):
-        return 1
+        rslt = 0
+        i = 0
+        while i < len(s):
+            if s[i] == 'I':
+                if i != len(s) - 1 and s[i+1] in 'VX':
+                    rslt += 4 if s[i+1] == 'V' else 9
+                    i += 2
+                    continue
+                rslt += 1
+            if s[i] == 'X':
+                if i != len(s) - 1 and s[i+1] in 'LC':
+                    rslt += 40 if s[i+1] == 'L' else 90
+                    i += 2
+                    continue
+                rslt += 10
+            if s[i] == 'C':
+                if i != len(s) - 1 and s[i+1] in 'DM':
+                    rslt += 400 if s[i+1] == 'D' else 900
+                    i += 2
+                    continue
+                rslt += 100
+            if s[i] == 'M':
+                rslt += 1000
+            if s[i] == 'V':
+                rslt += 5
+            if s[i] == 'L':
+                rslt += 50
+            if s[i] == 'D':
+                rslt += 500
+            i += 1
+
+        return rslt
--- pythonversion/Roman2Integer/TestSolution.py
+++ pythonversion/Roman2Integer/TestSolution.py
@@ -31,6 +31,258 @@ class Test(unittest.TestCase):
         self.assertEqual(expRslt, actRslt)
 
 
+    def testRomanToIntWhenSisII(self):
+        # arrange
+        s = 'II'
+        expRslt = 2
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisIII(self):
+        # arrange
+        s = 'III'
+        expRslt = 3
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisX(self):
+        # arrange
+        s = 'X'
+        expRslt = 10
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisXX(self):
+        # arrange
+        s = 'XX'
+        expRslt = 20
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisXXX(self):
+        # arrange
+        s = 'XXX'
+        expRslt = 30
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisC(self):
+        # arrange
+        s = 'C'
+        expRslt = 100
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisCC(self):
+        # arrange
+        s = 'CC'
+        expRslt = 200
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisCCC(self):
+        # arrange
+        s = 'CCC'
+        expRslt = 300
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisM(self):
+        # arrange
+        s = 'M'
+        expRslt = 1000
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisMMM(self):
+        # arrange
+        s = 'MMM'
+        expRslt = 3000
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisV(self):
+        # arrange
+        s = 'V'
+        expRslt = 5
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisL(self):
+        # arrange
+        s = 'L'
+        expRslt = 50
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisD(self):
+        # arrange
+        s = 'D'
+        expRslt = 500
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisIV(self):
+        # arrange
+        s = 'IV'
+        expRslt = 4
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisIX(self):
+        # arrange
+        s = 'IX'
+        expRslt = 9
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisXL(self):
+        # arrange
+        s = 'XL'
+        expRslt = 40
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisXC(self):
+        # arrange
+        s = 'XC'
+        expRslt = 90
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisCD(self):
+        # arrange
+        s = 'CD'
+        expRslt = 400
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisCM(self):
+        # arrange
+        s = 'CM'
+        expRslt = 900
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisMMMDCCCLXXXVIII(self):
+        # arrange
+        s = 'MMMDCCCLXXXVIII'
+        expRslt = 3888
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRomanToIntWhenSisMMMCMXCIX(self):
+        # arrange
+        s = 'MMMCMXCIX'
+        expRslt = 3999
+
+        # act
+        actRslt = self.testedobj.romanToInt(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
