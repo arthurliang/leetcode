ceb59022987ca9fb28bb76d838a7516b7f0112cc
Arthur Liang <arthurliang5@gmail.com>
2015-01-07 14:27:36 +0800

    add solution for LongestCommonPrefix

--- pythonversion/LongestCommonPrefix/Solution.py
+++ pythonversion/LongestCommonPrefix/Solution.py
@@ -3,4 +3,11 @@
 class Solution:
     # @return a string
     def longestCommonPrefix(self, strs):
-        return ""
+        if not strs:
+            return ""
+
+        for i, letter_group in enumerate(zip(*strs)):
+            if len(set(letter_group)) > 1:
+                return strs[0][:i]
+
+        return min(strs)
--- pythonversion/LongestCommonPrefix/TestSolution.py
+++ pythonversion/LongestCommonPrefix/TestSolution.py
@@ -31,6 +31,90 @@ class Test(unittest.TestCase):
         self.assertEqual(expRslt, actRslt)
 
 
+    def testlongestCommonPrefix_TC2(self):
+        # arrange
+        strs = ["a"]
+        expRslt = "a"
+
+        # act
+        actRslt = self.testedobj.longestCommonPrefix(strs)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testlongestCommonPrefix_TC3(self):
+        # arrange
+        strs = ["c","c"]
+        expRslt = "c"
+
+        # act
+        actRslt = self.testedobj.longestCommonPrefix(strs)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testlongestCommonPrefix_TC4(self):
+        # arrange
+        strs = ["c","ca"]
+        expRslt = "c"
+
+        # act
+        actRslt = self.testedobj.longestCommonPrefix(strs)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testlongestCommonPrefix_TC5(self):
+        # arrange
+        strs = ["ca","c"]
+        expRslt = "c"
+
+        # act
+        actRslt = self.testedobj.longestCommonPrefix(strs)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testlongestCommonPrefix_TC6(self):
+        # arrange
+        strs = ["ca","cb"]
+        expRslt = "c"
+
+        # act
+        actRslt = self.testedobj.longestCommonPrefix(strs)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testlongestCommonPrefix_TC7(self):
+        # arrange
+        strs = ["ca","cbd"]
+        expRslt = "c"
+
+        # act
+        actRslt = self.testedobj.longestCommonPrefix(strs)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testlongestCommonPrefix_TC8(self):
+        # arrange
+        strs = ["cae","cadf","cag"]
+        expRslt = "ca"
+
+        # act
+        actRslt = self.testedobj.longestCommonPrefix(strs)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
