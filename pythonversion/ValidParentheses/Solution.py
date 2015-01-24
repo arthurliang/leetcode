2d3ce30301f2d2bb599aa9cf5a991dd2e48d115d
Arthur Liang <arthurliang5@gmail.com>
2015-01-24 17:18:01 +0800

    add solution for ValidParentheses

--- pythonversion/ValidParentheses/Solution.py
+++ pythonversion/ValidParentheses/Solution.py
@@ -1,7 +1,24 @@
 # Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 # The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
 
+VALIDPAIR = {')':'(', '}':'{', ']':'['}
+
 class Solution:
     # @return a boolean
     def isValid(self, s):
-        return False
+        stack = []
+
+        for i in range(0, len(s)):
+            if s[i] == ')' or s[i] == '}' or s[i] == ']':
+                if len(stack) == 0:
+                    return False
+                else:
+                    lpair = stack.pop()
+                    if lpair != VALIDPAIR.get(s[i]):
+                        return False
+                    else:
+                        continue
+            else:
+                stack.append(s[i])
+
+        return not len(stack)
--- pythonversion/ValidParentheses/TestSolution.py
+++ pythonversion/ValidParentheses/TestSolution.py
@@ -31,6 +31,187 @@ class Test(unittest.TestCase):
         self.assertEqual(expRslt, actRslt)
 
 
+    def testIsValid_TC2(self):
+        # arrange
+        s = '()'
+        expRslt = True
+
+        # act
+        actRslt = self.testedobj.isValid(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testIsValid_TC3(self):
+        # arrange
+        s = '{'
+        expRslt = False
+
+        # act
+        actRslt = self.testedobj.isValid(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testIsValid_TC4(self):
+        # arrange
+        s = '{}'
+        expRslt = True
+
+        # act
+        actRslt = self.testedobj.isValid(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testIsValid_TC5(self):
+        # arrange
+        s = '['
+        expRslt = False
+
+        # act
+        actRslt = self.testedobj.isValid(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testIsValid_TC6(self):
+        # arrange
+        s = '[]'
+        expRslt = True
+
+        # act
+        actRslt = self.testedobj.isValid(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testIsValid_TC7(self):
+        # arrange
+        s = ')'
+        expRslt = False
+
+        # act
+        actRslt = self.testedobj.isValid(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testIsValid_TC8(self):
+        # arrange
+        s = '}'
+        expRslt = False
+
+        # act
+        actRslt = self.testedobj.isValid(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testIsValid_TC9(self):
+        # arrange
+        s = ']'
+        expRslt = False
+
+        # act
+        actRslt = self.testedobj.isValid(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testIsValid_TC10(self):
+        # arrange
+        s = '([])'
+        expRslt = True
+
+        # act
+        actRslt = self.testedobj.isValid(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testIsValid_TC11(self):
+        # arrange
+        s = '({})'
+        expRslt = True
+
+        # act
+        actRslt = self.testedobj.isValid(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testIsValid_TC12(self):
+        # arrange
+        s = '([{}])'
+        expRslt = True
+
+        # act
+        actRslt = self.testedobj.isValid(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testIsValid_TC13(self):
+        # arrange
+        s = '({[]})'
+        expRslt = True
+
+        # act
+        actRslt = self.testedobj.isValid(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testIsValid_TC14(self):
+        # arrange
+        s = '([]})'
+        expRslt = False
+
+        # act
+        actRslt = self.testedobj.isValid(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testIsValid_TC15(self):
+        # arrange
+        s = '({[])'
+        expRslt = False
+
+        # act
+        actRslt = self.testedobj.isValid(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testIsValid_TC16(self):
+        # arrange
+        s = '(){}[]()'
+        expRslt = True
+
+        # act
+        actRslt = self.testedobj.isValid(s)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
