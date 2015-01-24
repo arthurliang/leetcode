3030c1026da6058460389bb910e6395326f377d6
Arthur Liang <arthurliang5@gmail.com>
2015-01-24 23:10:38 +0800

    init PascalsTriangle package

---
+++ pythonversion/PascalsTriangle/TestSolution.py
@@ -0,0 +1,36 @@
+'''
+Created on Jan 24, 2015
+
+@author: Arthur
+'''
+import unittest
+import Solution
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        self.testedobj = Solution.Solution()
+        pass
+
+
+    def tearDown(self):
+        self.testedobj = None
+        pass
+
+
+    def testConvertToTitle_TC1(self):
+        # arrange
+        numRows = 1
+        expRslt = [[1]]
+
+        # act
+        actRslt = self.testedobj.generate(numRows)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
