ade9d7b3ac48c38ff9543bcfd771991436f538eb
Arthur Liang <arthurliang5@gmail.com>
2015-01-26 19:34:09 +0800

    add solution for RemoveElement

--- pythonversion/RemoveElement/Solution.py
+++ pythonversion/RemoveElement/Solution.py
@@ -7,4 +7,14 @@ class Solution:
     # @param    elem    an integer, value need to be removed
     # @return an integer
     def removeElement(self, A, elem):
-        return 0
+        if len(A) == 0 or elem is None:
+            return 0
+
+        i = 0
+        while i < len(A):
+            if elem == A[i]:
+                del A[i]
+                continue
+            i += 1
+
+        return len(A)
--- pythonversion/RemoveElement/TestSolution.py
+++ pythonversion/RemoveElement/TestSolution.py
@@ -34,6 +34,66 @@ class Test(unittest.TestCase):
         self.assertEqual(expA, A)
 
 
+    def testRemoveElement_TC2(self):
+        # arrange
+        A = [1]
+        elem = None
+        expLen = 0
+        expA = [1]
+
+        # act
+        actLen = self.testedobj.removeElement(A, elem)
+
+        # assert
+        self.assertEqual(expLen, actLen)
+        self.assertEqual(expA, A)
+
+
+    def testRemoveElement_TC3(self):
+        # arrange
+        A = []
+        elem = None
+        expLen = 0
+        expA = []
+
+        # act
+        actLen = self.testedobj.removeElement(A, elem)
+
+        # assert
+        self.assertEqual(expLen, actLen)
+        self.assertEqual(expA, A)
+
+
+    def testRemoveElement_TC4(self):
+        # arrange
+        A = [1]
+        elem = 1
+        expLen = 0
+        expA = []
+
+        # act
+        actLen = self.testedobj.removeElement(A, elem)
+
+        # assert
+        self.assertEqual(expLen, actLen)
+        self.assertEqual(expA, A)
+
+
+    def testRemoveElement_TC5(self):
+        # arrange
+        A = [1, 1]
+        elem = 1
+        expLen = 0
+        expA = []
+
+        # act
+        actLen = self.testedobj.removeElement(A, elem)
+
+        # assert
+        self.assertEqual(expLen, actLen)
+        self.assertEqual(expA, A)
+
+
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
