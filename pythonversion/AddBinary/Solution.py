04d8cae5e54b878b2475ff267100e990949747b7
Arthur Liang <arthurliang5@gmail.com>
2015-02-06 23:34:52 +0800

    optimize solution for AddBinary

--- pythonversion/AddBinary/Solution.py
+++ pythonversion/AddBinary/Solution.py
@@ -10,25 +10,4 @@ class Solution:
     # @param b, a string
     # @return a string
     def addBinary(self, a, b):
-        if len(a) >= len(b):
-            b = b.zfill(len(a))
-        else:
-            a = a.zfill(len(b))
-
-        s = zip(*[a, b])
-        i = len(s) - 1
-        carry = 0
-        while i >= 0:
-            s[i] = int(s[i][0]) + int(s[i][1]) + carry
-            if s[i] == 3:
-                s[i], carry = '1', 1
-            elif s[i] == 2:
-                s[i], carry = '0', 1
-            elif s[i] == 1:
-                s[i], carry = '1', 0
-            else:
-                s[i], carry = '0', 0
-            i -= 1
-        if carry == 1:
-            return "1" + "".join(s)
-        return "".join(s)
+        return '{0:b}'.format(int(a, 2) + int(b, 2))
