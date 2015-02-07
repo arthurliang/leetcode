63fbcef191277e0f7e45a37d02de353e719d8234
Arthur Liang <arthurliang5@gmail.com>
2015-02-07 17:31:46 +0800

    init BinaryTreePostorderTraversal package with recursive solution

---
+++ pythonversion/BinaryTreePostorderTraversal/Solution.py
@@ -0,0 +1,36 @@
+# Given a binary tree, return the postorder traversal of its nodes' values.
+#
+# For example:
+# Given binary tree {1,#,2,3},
+#    1
+#     \
+#      2
+#     /
+#    3
+# return [3,2,1].
+#
+# Note: Recursive solution is trivial, could you do it iteratively?
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of integers
+    def postorderTraversal(self, root):
+        # solution recursive, just for try
+        rslt = []
+        if root is None:
+            return rslt
+
+        rslt.extend(self.postorderTraversal(root.left))
+
+        rslt.extend(self.postorderTraversal(root.right))
+
+        rslt.append(root.val)
+
+        return rslt
