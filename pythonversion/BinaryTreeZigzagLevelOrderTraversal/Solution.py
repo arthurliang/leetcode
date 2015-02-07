2b5708e38b0900456ae750644fa2083084d07918
Arthur Liang <arthurliang5@gmail.com>
2015-02-07 20:28:49 +0800

    add iterative solution which using two queue

--- pythonversion/BinaryTreeZigzagLevelOrderTraversal/Solution.py
+++ pythonversion/BinaryTreeZigzagLevelOrderTraversal/Solution.py
@@ -22,8 +22,41 @@
 #         self.left = None
 #         self.right = None
 
+from Queue import Queue
+
 class Solution:
     # @param root, a tree node
     # @return a list of lists of integers
     def zigzagLevelOrder(self, root):
-        return []
+        if root is None:
+            return []
+
+        rslt = []
+        curLevel = []
+        cq = [root]
+        nq = []
+        reverse = True
+        while len(cq) != 0:
+            node = cq.pop()
+            curLevel.append(node.val)
+
+            if reverse:
+                if node.left is not None:
+                    nq.append(node.left)
+                if node.right is not None:
+                    nq.append(node.right)
+            else:
+                if node.right is not None:
+                    nq.append(node.right)
+                if node.left is not None:
+                    nq.append(node.left)
+
+            if len(cq) == 0:
+                reverse = not reverse
+                t = cq
+                cq = nq
+                nq = t
+                rslt.append(curLevel)
+                curLevel = []
+
+        return rslt
--- pythonversion/BinaryTreeZigzagLevelOrderTraversal/TestSolution.py
+++ pythonversion/BinaryTreeZigzagLevelOrderTraversal/TestSolution.py
@@ -32,6 +32,128 @@ class Test(unittest.TestCase):
         self.assertEqual(expRst, actRslt)
 
 
+    def testZigzagLevelOrder_TC2(self):
+        # arrange
+        testdataTrue = "{1}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        expRst = [[1]]
+
+        # act
+        actRslt = self.testedobj.zigzagLevelOrder(bt.rootnode)
+
+        # assert
+        self.assertEqual(expRst, actRslt)
+
+
+    def testZigzagLevelOrder_TC3(self):
+        # arrange
+        testdataTrue = "{1,2}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        expRst = [[1],
+                  [2]
+                  ]
+
+        # act
+        actRslt = self.testedobj.zigzagLevelOrder(bt.rootnode)
+
+        # assert
+        self.assertEqual(expRst, actRslt)
+
+
+    def testZigzagLevelOrder_TC4(self):
+        # arrange
+        testdataTrue = "{1,#,2}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        expRst = [[1],
+                  [2]
+                  ]
+
+        # act
+        actRslt = self.testedobj.zigzagLevelOrder(bt.rootnode)
+
+        # assert
+        self.assertEqual(expRst, actRslt)
+
+
+    def testZigzagLevelOrder_TC5(self):
+        # arrange
+        testdataTrue = "{1,3,2}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        expRst = [[1],
+                  [2,3]
+                  ]
+
+        # act
+        actRslt = self.testedobj.zigzagLevelOrder(bt.rootnode)
+
+        # assert
+        self.assertEqual(expRst, actRslt)
+
+
+    def testZigzagLevelOrder_TC6(self):
+        # arrange
+        testdataTrue = "{3,9,20,#,#,15,7}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        expRst = [[3],
+                  [20,9],
+                  [15,7]
+                  ]
+
+        # act
+        actRslt = self.testedobj.zigzagLevelOrder(bt.rootnode)
+
+        # assert
+        self.assertEqual(expRst, actRslt)
+
+
+    def testZigzagLevelOrder_TC7(self):
+        # arrange
+        testdataTrue = "{1,2,3,4,#,#,5}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        expRst = [[1],
+                  [3,2],
+                  [4,5]
+                  ]
+
+        # act
+        actRslt = self.testedobj.zigzagLevelOrder(bt.rootnode)
+
+        # assert
+        self.assertEqual(expRst, actRslt)
+
+
+    def testZigzagLevelOrder_TC8(self):
+        # arrange
+        testdataTrue = "{0,2,4,1,#,3,-1,5,1,#,6,#,8}"
+        bt = BinTree.BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+
+        expRst = [[0],
+                  [4,2],
+                  [1,3,-1],
+                  [8,6,1,5]]
+
+
+        # act
+        actRslt = self.testedobj.zigzagLevelOrder(bt.rootnode)
+
+        # assert
+        self.assertEqual(expRst, actRslt)
+
+
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
