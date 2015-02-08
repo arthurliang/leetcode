72c24dfec53dbaa9787131a5f8bb9289ff650391
Arthur Liang <arthurliang5@gmail.com>
2015-02-08 16:35:25 +0800

    add solution for ConstructBTFromPreorderAndInrderTraversal
    2 solutions which one can pass OJ

--- pythonversion/ConstructBinaryTreefromPreorderandInorderTraversal/Solution.py
+++ pythonversion/ConstructBinaryTreefromPreorderandInorderTraversal/Solution.py
@@ -18,5 +18,45 @@ class Solution:
     # @param inorder, a list of integers
     # @return a tree node
     def buildTree(self, preorder, inorder):
+        # iterative solution
         if not preorder:
             return None
+
+        head = TreeNode(preorder[0])
+
+        curNode = head
+        flagRight = False
+        stack = [head]
+        i = 1
+        j = 0
+        while i < len(preorder):
+            if stack and stack[-1].val == inorder[j]:
+                curNode = stack.pop()
+                j += 1
+                flagRight = True
+            elif flagRight:
+                curNode.right = TreeNode(preorder[i])
+                curNode = curNode.right
+                stack.append(curNode)
+                i += 1
+                flagRight = False
+            else:
+                curNode.left = TreeNode(preorder[i])
+                curNode = curNode.left
+                stack.append(curNode)
+                i += 1
+
+        return head
+
+#         # recursive solution
+#         if not preorder:
+#             return None
+#
+#         pos = inorder.index(preorder[0])
+#
+#         head = TreeNode(preorder[0])
+#         head.left = self.buildTree(preorder[1:pos + 1], inorder[0: pos])
+#         head.right = self.buildTree(preorder[pos + 1:len(preorder)], inorder[pos + 1: len(inorder)])
+#
+#         return head
+
--- pythonversion/ConstructBinaryTreefromPreorderandInorderTraversal/TestSolution.py
+++ pythonversion/ConstructBinaryTreefromPreorderandInorderTraversal/TestSolution.py
@@ -23,15 +23,120 @@ class Test(unittest.TestCase):
 
     def testBuildTree_TC1(self):
         # arrange
+        expRslt = None
         preorder = []
         inorder = []
-        expRst = None
 
         # act
         actRslt = self.testedobj.buildTree(preorder, inorder)
 
         # assert
-        self.assertEqual(expRst, actRslt)
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testBuildTree_TC2(self):
+        # arrange
+        expRslt = "{1}"
+        preorder = [1]
+        inorder = [1]
+
+        # act
+        actRslt = self.testedobj.buildTree(preorder, inorder)
+
+        # assert
+        bt = BinTree.BinTree()
+        bt.rootnode = actRslt
+        self.assertEqual(expRslt, bt.SerializationOnOJ())
+
+
+    def testBuildTree_TC3(self):
+        # arrange
+        expRslt = "{1,2}"
+        preorder = [1, 2]
+        inorder = [2, 1]
+
+        # act
+        actRslt = self.testedobj.buildTree(preorder, inorder)
+
+        # assert
+        bt = BinTree.BinTree()
+        bt.rootnode = actRslt
+        self.assertEqual(expRslt, bt.SerializationOnOJ())
+
+
+    def testBuildTree_TC5(self):
+        # arrange
+        expRslt = "{1,#,2}"
+        preorder = [1, 2]
+        inorder = [1, 2]
+
+        # act
+        actRslt = self.testedobj.buildTree(preorder, inorder)
+
+        # assert
+        bt = BinTree.BinTree()
+        bt.rootnode = actRslt
+        self.assertEqual(expRslt, bt.SerializationOnOJ())
+
+
+    def testBuildTree_TC6(self):
+        # arrange
+        expRslt = "{1,2,3}"
+        preorder = [1,2,3]
+        inorder = [2,1,3]
+
+        # act
+        actRslt = self.testedobj.buildTree(preorder, inorder)
+
+        # assert
+        bt = BinTree.BinTree()
+        bt.rootnode = actRslt
+        self.assertEqual(expRslt, bt.SerializationOnOJ())
+
+
+    def testBuildTree_TC7(self):
+        # arrange
+        expRslt = "{1,2,#,3}"
+        preorder = [1,2,3]
+        inorder = [3,2,1]
+
+        # act
+        actRslt = self.testedobj.buildTree(preorder, inorder)
+
+        # assert
+        bt = BinTree.BinTree()
+        bt.rootnode = actRslt
+        self.assertEqual(expRslt, bt.SerializationOnOJ())
+
+
+    def testBuildTree_TC8(self):
+        # arrange
+        expRslt = "{1,#,2,#,3}"
+        preorder = [1,2,3]
+        inorder = [1,2,3]
+
+        # act
+        actRslt = self.testedobj.buildTree(preorder, inorder)
+
+        # assert
+        bt = BinTree.BinTree()
+        bt.rootnode = actRslt
+        self.assertEqual(expRslt, bt.SerializationOnOJ())
+
+
+    def testBuildTree_TC9(self):
+        # arrange
+        expRslt = "{1,#,2,3}"
+        preorder = [1,2,3]
+        inorder = [1,3,2]
+
+        # act
+        actRslt = self.testedobj.buildTree(preorder, inorder)
+
+        # assert
+        bt = BinTree.BinTree()
+        bt.rootnode = actRslt
+        self.assertEqual(expRslt, bt.SerializationOnOJ())
 
 
 if __name__ == "__main__":
