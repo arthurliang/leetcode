4b91314dc4b1e6924b5e259284754b3464b9b0df
Arthur Liang <arthurliang5@gmail.com>
2015-02-08 16:38:27 +0800

    init ConstructBinaryTreefromInorderandPostorderTraversal pacakge

---
+++ pythonversion/ConstructBinaryTreefromInorderandPostorderTraversal/Solution.py
@@ -0,0 +1,23 @@
+# Given inorder and postorder traversal of a tree, construct the binary tree.
+#
+# Note:
+# You may assume that duplicates do not exist in the tree.
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+from utility.BinTree import TreeNode
+
+class Solution:
+    # @param inorder, a list of integers
+    # @param postorder, a list of integers
+    # @return a tree node
+    def buildTree(self, inorder, postorder):
+        if not inorder:
+            return None
+
