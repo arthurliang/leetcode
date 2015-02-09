c7fa465ce85c90f1ffd45ea65c652de0e6d29c74
Arthur Liang <arthurliang5@gmail.com>
2015-02-09 11:42:41 +0800

    init PopulatingNextRightPointersinEachNodeII package

---
+++ pythonversion/PopulatingNextRightPointersinEachNodeII/TestSolution.py
@@ -0,0 +1,35 @@
+'''
+Created on Feb 9, 2015
+
+@author: Arthur
+'''
+import unittest
+import Solution
+from utility.BinTree import BinTree
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        self.testedobj = Solution.Solution()
+        pass
+
+
+    def tearDown(self):
+        self.testedobj = None
+        pass
+
+
+    def testConnect_TC1(self):
+        # arrange
+
+        # act
+        self.testedobj.connect(None)
+
+        # assert
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
