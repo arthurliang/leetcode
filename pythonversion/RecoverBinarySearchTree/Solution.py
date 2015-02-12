c04f8c50c949c0afe03ebb76a21e374a4cda219e
Arthur Liang <arthurliang5@gmail.com>
2015-02-12 17:10:54 +0800

    add solution for RecoverBinarySearchTree

--- pythonversion/RecoverBinarySearchTree/Solution.py
+++ pythonversion/RecoverBinarySearchTree/Solution.py
@@ -13,8 +13,53 @@
 #         self.left = None
 #         self.right = None
 
+from utility.BinTree import TreeNode
+
 class Solution:
     # @param root, a tree node
     # @return a tree node
     def recoverTree(self, root):
+        if root is None:
+            return root
+
+        self.fstWrong = self.secWrong = TreeNode(None)
+
+#         # Below code can replace upper line when in OJ
+#         self.fstWrong = root
+#         while self.fstWrong.left:
+#             self.fstWrong = self.fstWrong.left
+#         self.secWrong = self.fstWrong
+
+        self.fstWrongLocked = self.secWrongLocked = False
+
+        self.findWrongByInorderTraversal(root)
+
+        if self.fstWrongLocked:
+            t = self.fstWrong.val
+            self.fstWrong.val = self.secWrong.val
+            self.secWrong.val = t
+
         return root
+
+    def findWrongByInorderTraversal(self, root):
+        if self.fstWrongLocked and self.secWrongLocked:
+            return
+
+        if root.left is not None:
+            self.findWrongByInorderTraversal(root.left)
+
+        if not self.fstWrongLocked:
+            if self.fstWrong.val <= root.val:
+                self.fstWrong = root
+            else:
+                self.secWrong = root
+                self.fstWrongLocked = True
+
+        if not self.secWrongLocked:
+            if self.secWrong.val < self.fstWrong.val and self.fstWrong.val < root.val:
+                self.secWrongLocked = True
+            else:
+                self.secWrong = root
+
+        if root.right is not None:
+            self.findWrongByInorderTraversal(root.right)
--- pythonversion/RecoverBinarySearchTree/TestSolution.py
+++ pythonversion/RecoverBinarySearchTree/TestSolution.py
@@ -35,6 +35,103 @@ class Test(unittest.TestCase):
         self.assertEqual(expRslt, actRslt)
 
 
+    def testRecoverTree_TC2(self):
+        testdataTrue = "{1}"
+        bt = BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+        expRslt = "{1}"
+
+        # act
+        bt.rootnode = self.testedobj.recoverTree(bt.rootnode)
+
+        # assert
+        actRslt = bt.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRecoverTree_TC3(self):
+        testdataTrue = "{0,1}"
+        bt = BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+        expRslt = "{1,0}"
+
+        # act
+        bt.rootnode = self.testedobj.recoverTree(bt.rootnode)
+
+        # assert
+        actRslt = bt.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRecoverTree_TC4(self):
+        testdataTrue = "{1,#,0}"
+        bt = BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+        expRslt = "{0,#,1}"
+
+        # act
+        bt.rootnode = self.testedobj.recoverTree(bt.rootnode)
+
+        # assert
+        actRslt = bt.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRecoverTree_TC5(self):
+        testdataTrue = "{0,1,2}"
+        bt = BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+        expRslt = "{1,0,2}"
+
+        # act
+        bt.rootnode = self.testedobj.recoverTree(bt.rootnode)
+
+        # assert
+        actRslt = bt.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRecoverTree_TC6(self):
+        testdataTrue = "{2,0,1}"
+        bt = BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+        expRslt = "{1,0,2}"
+
+        # act
+        bt.rootnode = self.testedobj.recoverTree(bt.rootnode)
+
+        # assert
+        actRslt = bt.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRecoverTree_TC7(self):
+        testdataTrue = "{1,2,0}"
+        bt = BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+        expRslt = "{1,0,2}"
+
+        # act
+        bt.rootnode = self.testedobj.recoverTree(bt.rootnode)
+
+        # assert
+        actRslt = bt.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testRecoverTree_TC8(self):
+        testdataTrue = "{20,15,25,10,22,17}"
+        bt = BinTree()
+        bt.DeserializationOnOJ(testdataTrue)
+        expRslt = "{20,15,25,10,17,22}"
+
+        # act
+        bt.rootnode = self.testedobj.recoverTree(bt.rootnode)
+
+        # assert
+        actRslt = bt.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
