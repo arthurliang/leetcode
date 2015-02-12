8821a41c7f28d5cb5b2e662f574a7e43eb517e9e
Arthur Liang <arthurliang5@gmail.com>
2015-02-13 00:29:23 +0800

    add solution for PartitionList

--- pythonversion/PartitionList/Solution.py
+++ pythonversion/PartitionList/Solution.py
@@ -14,9 +14,31 @@
 #         self.val = x
 #         self.next = None
 
+from utility.CustomList import ListNode
+
 class Solution:
     # @param head, a ListNode
     # @param x, an integer
     # @return a ListNode
     def partition(self, head, x):
-        return None
+        rslt = ListNode(0)
+        rslt.next = head
+
+        less = rslt
+        greater = None
+
+        while head:
+            if head.val >= x:
+                greater = head
+                head = head.next
+            elif greater is None:
+                less = head
+                head = head.next
+            else:
+                greater.next = head.next
+                head.next = less.next
+                less.next = head
+                less = head
+                head = greater.next
+
+        return rslt.next
--- pythonversion/PartitionList/TestSolution.py
+++ pythonversion/PartitionList/TestSolution.py
@@ -37,6 +37,102 @@ class Test(unittest.TestCase):
         self.assertEqual(expRslt, actRslt)
 
 
+    def testPartition_TC2(self):
+        #arrange
+        head = "{1}"
+        self.sllA.DeserializationOnOJ(head)
+        x = 0
+
+        expRslt = "{1}"
+
+        #act
+        self.sllA.listhead = self.testedobj.partition(self.sllA.listhead, x)
+
+        #assert
+        actRslt = self.sllA.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testPartition_TC3(self):
+        #arrange
+        head = "{2,1}"
+        self.sllA.DeserializationOnOJ(head)
+        x = 0
+
+        expRslt = "{2,1}"
+
+        #act
+        self.sllA.listhead = self.testedobj.partition(self.sllA.listhead, x)
+
+        #assert
+        actRslt = self.sllA.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testPartition_TC4(self):
+        #arrange
+        head = "{2,1}"
+        self.sllA.DeserializationOnOJ(head)
+        x = 1
+
+        expRslt = "{2,1}"
+
+        #act
+        self.sllA.listhead = self.testedobj.partition(self.sllA.listhead, x)
+
+        #assert
+        actRslt = self.sllA.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testPartition_TC5(self):
+        #arrange
+        head = "{2,1}"
+        self.sllA.DeserializationOnOJ(head)
+        x = 2
+
+        expRslt = "{1,2}"
+
+        #act
+        self.sllA.listhead = self.testedobj.partition(self.sllA.listhead, x)
+
+        #assert
+        actRslt = self.sllA.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testPartition_TC6(self):
+        #arrange
+        head = "{2,1}"
+        self.sllA.DeserializationOnOJ(head)
+        x = 3
+
+        expRslt = "{2,1}"
+
+        #act
+        self.sllA.listhead = self.testedobj.partition(self.sllA.listhead, x)
+
+        #assert
+        actRslt = self.sllA.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testPartition_TC7(self):
+        #arrange
+        head = "{1,4,3,2,5,2}"
+        self.sllA.DeserializationOnOJ(head)
+        x = 3
+
+        expRslt = "{1,2,2,4,3,5}"
+
+        #act
+        self.sllA.listhead = self.testedobj.partition(self.sllA.listhead, x)
+
+        #assert
+        actRslt = self.sllA.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
