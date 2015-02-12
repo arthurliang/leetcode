e12474bf85b6123aa5485056f87cd65c25b53fa6
Arthur Liang <arthurliang5@gmail.com>
2015-02-12 19:25:13 +0800

    add solution for ConvertSortedArraytoBST

--- pythonversion/ConvertSortedArraytoBinarySearchTree/Solution.py
+++ pythonversion/ConvertSortedArraytoBinarySearchTree/Solution.py
@@ -8,8 +8,28 @@
 #         self.left = None
 #         self.right = None
 
+from utility.BinTree import TreeNode
+
 class Solution:
     # @param num, a list of integers
     # @return a tree node
     def sortedArrayToBST(self, num):
-        return None
+        if num is None or len(num) == 0:
+            return None
+
+        root = TreeNode(0)
+        stack = [(root, 0, len(num) - 1)]
+
+        while stack:
+            node, start, end = stack.pop()
+            mid = (start + end) / 2
+            node.val = num[mid]
+            if mid - 1 >= start:
+                node.left = TreeNode(0)
+                stack.append((node.left, start, mid - 1))
+            if mid + 1 <= end:
+                node.right = TreeNode(0)
+                stack.append((node.right, mid + 1, end))
+
+        return root
+
--- pythonversion/ConvertSortedArraytoBinarySearchTree/TestSolution.py
+++ pythonversion/ConvertSortedArraytoBinarySearchTree/TestSolution.py
@@ -23,10 +23,100 @@ class Test(unittest.TestCase):
 
     def testSortedArrayToBST_TC1(self):
         # arrange
+        testdata = None
+        bt = BinTree()
+
+        expRslt = "{}"
+
+        # act
+        bt.rootnode = self.testedobj.sortedArrayToBST(testdata)
+
+        # assert
+        actRslt = bt.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSortedArrayToBST_TC2(self):
+        # arrange
         testdata = []
         bt = BinTree()
 
         expRslt = "{}"
+
+        # act
+        bt.rootnode = self.testedobj.sortedArrayToBST(testdata)
+
+        # assert
+        actRslt = bt.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSortedArrayToBST_TC3(self):
+        # arrange
+        testdata = [1]
+        bt = BinTree()
+
+        expRslt = "{1}"
+
+        # act
+        bt.rootnode = self.testedobj.sortedArrayToBST(testdata)
+
+        # assert
+        actRslt = bt.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSortedArrayToBST_TC4(self):
+        # arrange
+        testdata = [1,2]
+        bt = BinTree()
+
+        expRslt = "{1,#,2}"
+
+        # act
+        bt.rootnode = self.testedobj.sortedArrayToBST(testdata)
+
+        # assert
+        actRslt = bt.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSortedArrayToBST_TC5(self):
+        # arrange
+        testdata = [1,2,3]
+        bt = BinTree()
+
+        expRslt = "{2,1,3}"
+
+        # act
+        bt.rootnode = self.testedobj.sortedArrayToBST(testdata)
+
+        # assert
+        actRslt = bt.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSortedArrayToBST_TC6(self):
+        # arrange
+        testdata = [1,2,3,4]
+        bt = BinTree()
+
+        expRslt = "{2,1,3,#,#,#,4}"
+
+        # act
+        bt.rootnode = self.testedobj.sortedArrayToBST(testdata)
+
+        # assert
+        actRslt = bt.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSortedArrayToBST_TC7(self):
+        # arrange
+        testdata = [0,1,2,3,4]
+        bt = BinTree()
+
+        expRslt = "{2,0,3,#,1,#,4}"
 
         # act
         bt.rootnode = self.testedobj.sortedArrayToBST(testdata)
