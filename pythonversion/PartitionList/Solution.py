4f55886c0d97715153580a319455ff433d5cd15a
Arthur Liang <arthurliang5@gmail.com>
2015-02-13 00:45:49 +0800

    add 2nd solution for PartitionList

--- pythonversion/PartitionList/Solution.py
+++ pythonversion/PartitionList/Solution.py
@@ -21,24 +21,46 @@ class Solution:
     # @param x, an integer
     # @return a ListNode
     def partition(self, head, x):
+        # 2nd solution
         rslt = ListNode(0)
-        rslt.next = head
+        temp = ListNode(1)
 
-        less = rslt
-        greater = None
+        lesslist = rslt
+        greatlist = temp
 
         while head:
-            if head.val >= x:
-                greater = head
-                head = head.next
-            elif greater is None:
-                less = head
-                head = head.next
+            if head.val < x:
+                lesslist.next = head
+                lesslist = lesslist.next
             else:
-                greater.next = head.next
-                head.next = less.next
-                less.next = head
-                less = head
-                head = greater.next
+                greatlist.next = head
+                greatlist = greatlist.next
 
+            head = head.next
+
+        greatlist.next = None
+        lesslist.next = temp.next
         return rslt.next
+
+#         # 1st solution
+#         rslt = ListNode(0)
+#         rslt.next = head
+#
+#         less = rslt
+#         greater = None
+#
+#         while head:
+#             if head.val >= x:
+#                 greater = head
+#                 head = head.next
+#             elif greater is None:
+#                 less = head
+#                 head = head.next
+#             else:
+#                 greater.next = head.next
+#                 head.next = less.next
+#                 less.next = head
+#                 less = head
+#                 head = greater.next
+#
+#         return rslt.next
