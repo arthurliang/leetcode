06cd6e5d6ce228fb49df3766dfed0bbf07769095
Arthur Liang <arthurliang5@gmail.com>
2015-02-15 17:43:57 +0800

    not optimized binsearch solution

--- pythonversion/SearchinRotatedSortedArray/Solution.py
+++ pythonversion/SearchinRotatedSortedArray/Solution.py
@@ -11,4 +11,64 @@ class Solution:
     # @param target, an integer to be searched
     # @return an integer
     def search(self, A, target):
-        return -1
+        if not A or len(A) == 0:
+            return -1
+
+        return self.searchExt(A, 0, len(A), target)
+
+    # @param A, a list of integers
+    # @param start: start index
+    # @param end: end index, exclusive
+    # @param target, an integer to be searched
+    # @return an integer
+    def searchExt(self, A, start, end, target):
+        if start > end - 1:
+            return -1
+
+        if A[start] < A[end - 1]:
+            return self.binsearch(A, start, end, target)
+
+        mid = (end + start) / 2
+
+        if target < A[start]:
+            if A[mid] < target:
+                return self.binsearch(A, mid + 1, end, target)
+            elif A[mid] > target:
+                if A[mid] > A[start]:
+                    return self.searchExt(A, mid + 1, end, target)
+                else:
+                    return self.searchExt(A, start + 1, mid, target)
+            else:
+                return mid
+
+        elif target > A[start]:
+            if A[mid] > target:
+                return self.binsearch(A, start + 1, mid, target)
+
+            elif A[mid] < target:
+                if A[mid] > A[start]:
+                    return self.searchExt(A, mid + 1, end, target)
+                else:
+                    return self.searchExt(A, start + 1, mid, target)
+            else:
+                return mid
+        else:
+            return start
+
+
+    # @param A, a list of integers
+    # @param start: start index
+    # @param end: end index, exclusive
+    # @param target, an integer to be searched
+    # @return an integer
+    def binsearch(self, A, start, end, target):
+        if end - 1 < start:
+            return -1
+
+        mid = (end + start) / 2
+        if A[mid] > target:
+            return self.binsearch(A, start, mid, target)
+        elif A[mid] < target:
+            return self.binsearch(A, mid + 1, end, target)
+        else:
+            return mid
--- pythonversion/SearchinRotatedSortedArray/TestSolution.py
+++ pythonversion/SearchinRotatedSortedArray/TestSolution.py
@@ -32,6 +32,199 @@ class Test(unittest.TestCase):
         self.assertEqual(expRslt, actRslt)
 
 
+    def testSearch_TC2(self):
+        # arrange
+        A = None
+        target = 0
+        expRslt = -1
+
+        # act
+        actRslt = self.testedobj.search(A, target)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSearch_TC3(self):
+        # arrange
+        A = [1]
+        target = 1
+        expRslt = 0
+
+        # act
+        actRslt = self.testedobj.search(A, target)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSearch_TC4(self):
+        # arrange
+        A = [1,0]
+        target = 0
+        expRslt = 1
+
+        # act
+        actRslt = self.testedobj.search(A, target)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSearch_TC5(self):
+        # arrange
+        A = [1,-1,0]
+        target = 0
+        expRslt = 2
+
+        # act
+        actRslt = self.testedobj.search(A, target)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSearch_TC6(self):
+        # arrange
+        A = [1,2,0]
+        target = 0
+        expRslt = 2
+
+        # act
+        actRslt = self.testedobj.search(A, target)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSearch_TC7(self):
+        # arrange
+        A = [1,2,0]
+        target = -2
+        expRslt = -1
+
+        # act
+        actRslt = self.testedobj.search(A, target)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSearch_TC8(self):
+        # arrange
+        A = [1,3,0]
+        target = 2
+        expRslt = -1
+
+        # act
+        actRslt = self.testedobj.search(A, target)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSearch_TC9(self):
+        # arrange
+        A = [1,2,0]
+        target = 2
+        expRslt = 1
+
+        # act
+        actRslt = self.testedobj.search(A, target)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSearch_TC10(self):
+        # arrange
+        A = [0,1,-1]
+        target = 2
+        expRslt = -1
+
+        # act
+        actRslt = self.testedobj.search(A, target)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSearch_TC11(self):
+        # arrange
+        A = [0,1,-3,-2,-1]
+        target = 1
+        expRslt = 1
+
+        # act
+        actRslt = self.testedobj.search(A, target)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSearch_TC12(self):
+        # arrange
+        A = [5,1,3]
+        target = 2
+        expRslt = -1
+
+        # act
+        actRslt = self.testedobj.search(A, target)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSearch_TC13(self):
+        # arrange
+        A = [1,3,5]
+        target = 5
+        expRslt = 2
+
+        # act
+        actRslt = self.testedobj.search(A, target)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSearch_TC14(self):
+        # arrange
+        A = [4,5,6,7,0,1,2]
+        target = 0
+        expRslt = 4
+
+        # act
+        actRslt = self.testedobj.search(A, target)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSearch_TC15(self):
+        # arrange
+        A = [7,8,1,2,3,4,5,6]
+        target = 2
+        expRslt = 3
+
+        # act
+        actRslt = self.testedobj.search(A, target)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSearch_TC16(self):
+        # arrange
+        A = [7,8,9,10,11,12,5,6]
+        target = 12
+        expRslt = 5
+
+        # act
+        actRslt = self.testedobj.search(A, target)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
 
 
 if __name__ == "__main__":
