339bb607d77a48d3287f8f280f1dedf17dab1812
Arthur Liang <arthurliang5@gmail.com>
2015-02-15 15:37:54 +0800

    add solution which using merge sort

--- pythonversion/SortList/Solution.py
+++ pythonversion/SortList/Solution.py
@@ -6,8 +6,85 @@
 #         self.val = x
 #         self.next = None
 
+from utility.CustomList import ListNode
+
 class Solution:
     # @param head, a ListNode
     # @return a ListNode
     def sortList(self, head):
-        return None
+        if head is None or head.next is None:
+            return head
+
+        newhead = ListNode(-1)
+        tmphead = ListNode(-1)
+
+        newhead.next = head
+
+        step = 1
+        finished = False
+        while not finished:
+            head = newhead.next
+            firstIter = True
+            pre = None
+            prelast = None
+            while head:
+                ha = head
+                for i in range(step):
+                    if head:
+                        pre = head
+                        head = head.next
+                    else:
+                        break
+
+                if head is None:
+                    if firstIter:
+                        finished = True
+                    break
+
+                pre.next = None
+
+                hb = head
+                for i in range(step):
+                    if head:
+                        pre = head
+                        head = head.next
+                    else:
+                        break
+                pre.next = None
+
+                last = self.bottomUpMerge(ha, hb, tmphead)
+                last.next = head
+
+                if prelast:
+                    prelast.next = tmphead.next
+                prelast = last
+
+                if firstIter:
+                    newhead.next = tmphead.next
+                    firstIter = False
+
+            step *= 2
+
+        return newhead.next
+
+    def bottomUpMerge(self, ha, hb, newHead):
+        t = newHead
+
+        while ha and hb:
+            if ha.val < hb.val:
+                t.next = ha
+                ha = ha.next
+            else:
+                t.next = hb
+                hb = hb.next
+            t = t.next
+
+        if ha is not None:
+            t.next = ha
+        if hb is not None:
+            t.next = hb
+
+        while t and t.next:
+            t = t.next
+        return t
+
--- pythonversion/SortList/TestSolution.py
+++ pythonversion/SortList/TestSolution.py
@@ -32,6 +32,81 @@ class Test(unittest.TestCase):
         self.assertEqual(expRslt, actRslt)
 
 
+    def testSortList_TC2(self):
+        #arrange
+        testData = "{1}"
+        self.sllA.DeserializationOnOJ(testData)
+
+        expRslt = "{1}"
+
+        #act
+        self.sllA.listhead = self.testedobj.sortList(self.sllA.listhead)
+
+        #assert
+        actRslt = self.sllA.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSortList_TC3(self):
+        #arrange
+        testData = "{2,1}"
+        self.sllA.DeserializationOnOJ(testData)
+
+        expRslt = "{1,2}"
+
+        #act
+        self.sllA.listhead = self.testedobj.sortList(self.sllA.listhead)
+
+        #assert
+        actRslt = self.sllA.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSortList_TC4(self):
+        #arrange
+        testData = "{3,2,1}"
+        self.sllA.DeserializationOnOJ(testData)
+
+        expRslt = "{1,2,3}"
+
+        #act
+        self.sllA.listhead = self.testedobj.sortList(self.sllA.listhead)
+
+        #assert
+        actRslt = self.sllA.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSortList_TC5(self):
+        #arrange
+        testData = "{3,2,1,4}"
+        self.sllA.DeserializationOnOJ(testData)
+
+        expRslt = "{1,2,3,4}"
+
+        #act
+        self.sllA.listhead = self.testedobj.sortList(self.sllA.listhead)
+
+        #assert
+        actRslt = self.sllA.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSortList_TC6(self):
+        #arrange
+        testData = "{1,2,6,4,5,2,1}"
+        self.sllA.DeserializationOnOJ(testData)
+
+        expRslt = "{1,1,2,2,4,5,6}"
+
+        #act
+        self.sllA.listhead = self.testedobj.sortList(self.sllA.listhead)
+
+        #assert
+        actRslt = self.sllA.SerializationOnOJ()
+        self.assertEqual(expRslt, actRslt)
+
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
