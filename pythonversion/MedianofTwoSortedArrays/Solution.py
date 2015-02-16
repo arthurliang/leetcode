8c7c508b0b1b455d425a51fc714b0be28a1e2039
Arthur Liang <arthurliang5@gmail.com>
2015-02-16 10:06:54 +0800

    add solution for MedianofTwoSortedArrays

--- pythonversion/MedianofTwoSortedArrays/Solution.py
+++ pythonversion/MedianofTwoSortedArrays/Solution.py
@@ -1,8 +1,40 @@
-# There are two sorted arrays A and B of size m and n respectively.
 # Find the median of the two sorted arrays.
 # The overall run time complexity should be O(log (m+n)).
 
 class Solution:
     # @return a float
     def findMedianSortedArrays(self, A, B):
-        return 1
+        total = len(A) + len(B)
+
+        if (total & 1):
+            return self.findKth(A, B, total / 2 + 1)
+        else:
+            return float((self.findKth(A, B, total / 2) + self.findKth(A, B, total / 2 + 1))) / 2
+
+
+    # @param A: a list
+    # @param B: a list
+    # @param k: k-th number of smallest element in union of A and B
+    # @return: k-th smallest element in union of A and B
+    def findKth(self, A, B, k):
+        m = len(A)
+        n = len(B)
+
+        if m > n:
+            return self.findKth(B, A, k)
+
+        if m == 0:
+            return B[k - 1]
+
+        if k == 1:
+            return min(A[0], B[0])
+
+        pa = min(k / 2, m)
+        pb = k - pa
+
+        if A[pa - 1] < B[pb - 1]:
+            return self.findKth(A[pa:], B, k - pa)
+        elif A[pa - 1] > B[pb - 1]:
+            return self.findKth(A, B[pb:], k - pb)
+        else:
+            return A[pa - 1]
--- pythonversion/MedianofTwoSortedArrays/TestSolution.py
+++ pythonversion/MedianofTwoSortedArrays/TestSolution.py
@@ -22,8 +22,73 @@ class Test(unittest.TestCase):
     def testFindMedianSortedArrays_TC1(self):
         # arrange
         A = []
+        B = [2]
+        expRslt = 2
+
+        # act
+        actRslt = self.testedobj.findMedianSortedArrays(A, B)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testFindMedianSortedArrays_TC2(self):
+        # arrange
+        A = [3]
+        B = []
+        expRslt = 3
+
+        # act
+        actRslt = self.testedobj.findMedianSortedArrays(A, B)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testFindMedianSortedArrays_TC3(self):
+        # arrange
+        A = [3]
+        B = [1, 2]
+        expRslt = 2
+
+        # act
+        actRslt = self.testedobj.findMedianSortedArrays(A, B)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testFindMedianSortedArrays_TC4(self):
+        # arrange
+        A = [2]
         B = [1]
-        expRslt = 1
+        expRslt = 1.5
+
+        # act
+        actRslt = self.testedobj.findMedianSortedArrays(A, B)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testFindMedianSortedArrays_TC5(self):
+        # arrange
+        A = [2,3]
+        B = [1,5]
+        expRslt = 2.5
+
+        # act
+        actRslt = self.testedobj.findMedianSortedArrays(A, B)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testFindMedianSortedArrays_TC6(self):
+        # arrange
+        A = [2,3]
+        B = [1,4,5]
+        expRslt = 3
 
         # act
         actRslt = self.testedobj.findMedianSortedArrays(A, B)
@@ -33,5 +98,5 @@ class Test(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
+    # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
