68e0900042de850b554792b260f4e76e149f47d3
Arthur Liang <arthurliang5@gmail.com>
2015-02-16 22:58:10 +0800

    add backtrace solution for NQueens

--- pythonversion/NQueens/Solution.py
+++ pythonversion/NQueens/Solution.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# The n-queens puzzle is the problem of placing n queens on an n¡Án chessboard
+# The n-queens puzzle is the problem of placing n queens on an nï¿½ï¿½n chessboard
 # such that no two queens attack each other.
 #
 # Given an integer n, return all distinct solutions to the n-queens puzzle.
@@ -22,7 +22,69 @@
 #   ".Q.."]
 # ]
 
+# Memo for Queen's puzzle
+class Memo:
+    def __init__(self, n, row):
+        self.n = n
+        self.pos = 0
+        self.row = row
+        self.ancestor = None
+
 class Solution:
     # @return a list of lists of string
     def solveNQueens(self, n):
-        return [["Q"]]
+        self.result = []
+        self.backtrace(Memo(n, 0))
+        return self.result
+
+    def backtrace(self, memo):
+        for pos in range(memo.n):
+
+            memo.pos = pos
+            if self.reject(memo):
+                continue
+
+            memo.row += 1
+
+            if self.accept(memo):
+                self.outPut(memo)
+            else:
+                newMemo = Memo(memo.n, memo.row)
+                memo.row -= 1
+                newMemo.ancestor = memo
+                self.backtrace(newMemo)
+
+    def reject(self, memo):
+        if memo.ancestor is None:
+            return False
+
+        ancestor = memo.ancestor
+        posLeft = memo.pos
+        posRight = memo.pos
+        while ancestor:
+            if ancestor.pos == memo.pos:
+                break
+
+            posLeft -= 1
+            if ancestor.pos == posLeft:
+                break
+
+            posRight += 1
+            if ancestor.pos == posRight:
+                break
+
+            ancestor = ancestor.ancestor
+
+        if ancestor:
+            return True
+        return False
+
+    def accept(self, memo):
+        return memo.row == memo.n
+
+    def outPut(self, memo):
+        solution = []
+        while memo:
+            solution.insert(0,"".join(['.' if i != memo.pos else 'Q' for i in range(memo.n)]))
+            memo = memo.ancestor
+        self.result.append(solution)
--- pythonversion/NQueens/TestSolution.py
+++ pythonversion/NQueens/TestSolution.py
@@ -31,6 +31,51 @@ class Test(unittest.TestCase):
         self.assertEqual(expRslt, actRslt)
 
 
+    def testSolveNQueens_TC2(self):
+        # arrange
+        n = 2
+        expRslt = []
+
+        # act
+        actRslt = self.testedobj.solveNQueens(n)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSolveNQueens_TC3(self):
+        # arrange
+        n = 3
+        expRslt = []
+
+        # act
+        actRslt = self.testedobj.solveNQueens(n)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
+    def testSolveNQueens_TC4(self):
+        # arrange
+        n = 4
+        expRslt = [[".Q..",
+                    "...Q",
+                    "Q...",
+                    "..Q."],
+
+                    ["..Q.",
+                    "Q...",
+                    "...Q",
+                    ".Q.."]
+                   ]
+
+        # act
+        actRslt = self.testedobj.solveNQueens(n)
+
+        # assert
+        self.assertEqual(expRslt, actRslt)
+
+
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
+    # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
