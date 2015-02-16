11d33225832c99c338519e48dcc05e3945a7a8c5
Arthur Liang <arthurliang5@gmail.com>
2015-02-16 11:31:14 +0800

    a solution by using heapq lib

--- pythonversion/MergeKSortedLists/Solution.py
+++ pythonversion/MergeKSortedLists/Solution.py
@@ -8,36 +8,57 @@
 #         self.next = None
 
 from utility.CustomList import ListNode
+import heapq
 
 class Solution:
     # @param a list of ListNode
     # @return a ListNode
     def mergeKLists(self, lists):
-        if lists is None or len(lists) == 0:
+        # heapq from std lib solution
+        if lists is None:
             return None
 
-        sentinel = ListNode(-1)
+        c = sentinel = ListNode(-1)
 
-        while len(lists) != 1:
-            lists.insert(0, self.bottomUpMerge(lists.pop(), lists.pop(), sentinel))
+        minHeap = [(node.val, node) for node in lists if node]
+        heapq.heapify(minHeap)
 
-        return lists[0]
-
-
-    def bottomUpMerge(self, A, B, sentinel):
-        c = sentinel
-        while A and B:
-            if A.val <= B.val:
-                c.next = A
-                A = A.next
-            else:
-                c.next = B
-                B = B.next
+        while minHeap:
+            node = heapq.heappop(minHeap)[1]
+            c.next = node
             c = c.next
-
-        if A is None:
-            c.next = B
-        if B is None:
-            c.next = A
+            if c.next:
+                heapq.heappush(minHeap, (c.next.val, c.next))
 
         return sentinel.next
+
+
+#         # Merge Sort likely solution
+#         if lists is None or len(lists) == 0:
+#             return None
+#
+#         sentinel = ListNode(-1)
+#
+#         while len(lists) != 1:
+#             lists.insert(0, self.bottomUpMerge(lists.pop(), lists.pop(), sentinel))
+#
+#         return lists[0]
+#
+#
+#     def bottomUpMerge(self, A, B, sentinel):
+#         c = sentinel
+#         while A and B:
+#             if A.val <= B.val:
+#                 c.next = A
+#                 A = A.next
+#             else:
+#                 c.next = B
+#                 B = B.next
+#             c = c.next
+#
+#         if A is None:
+#             c.next = B
+#         if B is None:
+#             c.next = A
+#
+#         return sentinel.next
